3f42a1c750b03e10f881c3e3036f5a6106096a3c
Julien Dubois <julien.dubois@gmail.com>
2015-10-05 15:47:36 +0200

    automatic project update

--- src/main/java/com/mycompany/myapp/config/CacheConfiguration.java
+++ src/main/java/com/mycompany/myapp/config/CacheConfiguration.java
@@ -7,10 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.*;
 import org.springframework.cache.ehcache.EhCacheCacheManager;
 import org.springframework.util.Assert;
 
