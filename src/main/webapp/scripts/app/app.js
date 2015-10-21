501891a29456a4d5a74fc56e1c1d2006c43d5a76
Julien Dubois <julien.dubois@gmail.com>
2015-10-21 18:00:06 +0200

    automatic project update

--- src/main/webapp/scripts/app/app.js
+++ src/main/webapp/scripts/app/app.js
@@ -100,4 +100,14 @@ angular.module('sampleOAuth2App', ['LocalStorageModule', 'tmh.dynamicLocale', 'p
         tmhDynamicLocaleProvider.useCookieStorage();
         tmhDynamicLocaleProvider.storageKey('NG_TRANSLATE_LANG_KEY');
         
-    });
+    })
+    .config(['$urlMatcherFactoryProvider', function($urlMatcherFactory) {
+        $urlMatcherFactory.type('boolean', {
+            name : 'boolean',
+            decode: function(val) { return val == true ? true : val == "true" ? true : false },
+            encode: function(val) { return val ? 1 : 0; },
+            equals: function(a, b) { return this.is(a) && a === b; },
+            is: function(val) { return [true,false,0,1].indexOf(val) >= 0 },
+            pattern: /bool|true|0|1/
+        });
+    }]);;
