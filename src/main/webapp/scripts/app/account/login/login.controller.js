f0f9eefc3f98dc64d3e5c1e740beb7bdaf0d3d36
Julien Dubois <julien.dubois@gmail.com>
2015-11-04 17:40:47 +0100

    automatic project update

--- src/main/webapp/scripts/app/account/login/login.controller.js
+++ src/main/webapp/scripts/app/account/login/login.controller.js
@@ -2,17 +2,20 @@
 
 angular.module('sampleOAuth2App')
     .controller('LoginController', function ($rootScope, $scope, $state, $timeout, Auth) {
-        $scope.user = {};
-        $scope.errors = {};
+        $scope.credentials = {
+            username: null,
+            password: null,
+            rememberMe: true
+        };
+        $scope.authenticationError = false;
 
-        $scope.rememberMe = true;
         $timeout(function (){angular.element('[ng-model="username"]').focus();});
         $scope.login = function (event) {
             event.preventDefault();
             Auth.login({
-                username: $scope.username,
-                password: $scope.password,
-                rememberMe: $scope.rememberMe
+                username: $scope.credentials.username,
+                password: $scope.credentials.password,
+                rememberMe: $scope.credentials.rememberMe
             }).then(function () {
                 $scope.authenticationError = false;
                 if ($rootScope.previousStateName === 'register') {
