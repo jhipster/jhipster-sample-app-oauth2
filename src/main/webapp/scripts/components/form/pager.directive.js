366416df13bc40f9f7457500e5a23e3b21f195af
Julien Dubois <julien.dubois@gmail.com>
2016-02-10 10:35:02 +0100

    automatic project update

--- src/main/webapp/scripts/components/form/pager.directive.js
+++
@@ -1,9 +0,0 @@
-/* globals $ */
-'use strict';
-
-angular.module('sampleOAuth2App')
-    .directive('sampleOAuth2AppPager', function() {
-        return {
-            templateUrl: 'scripts/components/form/pager.html'
-        };
-    });
--- src/main/webapp/scripts/components/form/pagination.directive.js
+++
@@ -1,9 +0,0 @@
-/* globals $ */
-'use strict';
-
-angular.module('sampleOAuth2App')
-    .directive('sampleOAuth2AppPagination', function() {
-        return {
-            templateUrl: 'scripts/components/form/pagination.html'
-        };
-    });
