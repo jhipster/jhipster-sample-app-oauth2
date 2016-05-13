7d5587537737b9d67131dd2e6f919fdf470842b8
Julien Dubois <julien.dubois@gmail.com>
2016-05-13 16:55:27 +0200

    automatic project update

--- src/main/webapp/app/components/alert/alert-error.directive.js
+++ src/main/webapp/app/components/alert/alert-error.directive.js
@@ -9,7 +9,7 @@
                   '</div>',
         controller: jhiAlertErrorController
     };
-    
+
     angular
         .module('sampleOAuth2App')
         .component('jhiAlertError', jhiAlertError);
@@ -66,6 +66,10 @@
                 } else {
                     addErrorAlert(httpResponse.data);
                 }
+                break;
+
+            case 404:
+                addErrorAlert('Not found','error.url.not.found');
                 break;
 
             default:
