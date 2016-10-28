0a5fba266fae74a312428068a520439d19e8d244
Julien Dubois <julien.dubois@gmail.com>
2016-10-28 15:10:57 +0200

    automatic project update

--- src/main/webapp/app/blocks/config/http.config.js
+++ src/main/webapp/app/blocks/config/http.config.js
@@ -8,7 +8,6 @@
     httpConfig.$inject = ['$urlRouterProvider', '$httpProvider', 'httpRequestInterceptorCacheBusterProvider', '$urlMatcherFactoryProvider'];
 
     function httpConfig($urlRouterProvider, $httpProvider, httpRequestInterceptorCacheBusterProvider, $urlMatcherFactoryProvider) {
-        
         //Cache everything except rest api requests
         httpRequestInterceptorCacheBusterProvider.setMatchlist([/.*api.*/, /.*protected.*/], true);
 
