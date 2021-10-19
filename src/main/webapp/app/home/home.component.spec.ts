2e4919df92571f0416edc9f4226766b4ad071b5e
Pascal Grimaud <pascalgrimaud@gmail.com>
2021-10-19 13:09:00 +0200

    automatic project update

--- src/main/webapp/app/home/home.component.spec.ts
+++ src/main/webapp/app/home/home.component.spec.ts
@@ -11,64 +11,62 @@ import { LoginService } from 'app/login/login.service';
 
 import { HomeComponent } from './home.component';
 
-describe('Component Tests', () => {
-  describe('Home Component', () => {
-    let comp: HomeComponent;
-    let fixture: ComponentFixture<HomeComponent>;
-    let mockAccountService: AccountService;
-    let mockLoginService: LoginService;
-    const account: Account = {
-      activated: true,
-      authorities: [],
-      email: '',
-      firstName: null,
-      langKey: '',
-      lastName: null,
-      login: 'login',
-      imageUrl: null,
-    };
+describe('Home Component', () => {
+  let comp: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
+  let mockAccountService: AccountService;
+  let mockLoginService: LoginService;
+  const account: Account = {
+    activated: true,
+    authorities: [],
+    email: '',
+    firstName: null,
+    langKey: '',
+    lastName: null,
+    login: 'login',
+    imageUrl: null,
+  };
 
-    beforeEach(
-      waitForAsync(() => {
-        TestBed.configureTestingModule({
-          declarations: [HomeComponent],
-          providers: [AccountService, LoginService],
-        })
-          .overrideTemplate(HomeComponent, '')
-          .compileComponents();
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [HomeComponent],
+        providers: [AccountService, LoginService],
       })
-    );
+        .overrideTemplate(HomeComponent, '')
+        .compileComponents();
+    })
+  );
 
-    beforeEach(() => {
-      fixture = TestBed.createComponent(HomeComponent);
-      comp = fixture.componentInstance;
-      mockAccountService = TestBed.inject(AccountService);
-      mockAccountService.identity = jest.fn(() => of(null));
-      mockAccountService.getAuthenticationState = jest.fn(() => of(null));
-      mockLoginService = TestBed.inject(LoginService);
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HomeComponent);
+    comp = fixture.componentInstance;
+    mockAccountService = TestBed.inject(AccountService);
+    mockAccountService.identity = jest.fn(() => of(null));
+    mockAccountService.getAuthenticationState = jest.fn(() => of(null));
+    mockLoginService = TestBed.inject(LoginService);
+  });
+
+  describe('ngOnInit', () => {
+    it('Should synchronize account variable with current account', () => {
+      // GIVEN
+      mockAccountService.identity = jest.fn(() => of(account));
+
+      // WHEN
+      comp.ngOnInit();
+
+      // THEN
+      expect(comp.account).toEqual(account);
     });
+  });
 
-    describe('ngOnInit', () => {
-      it('Should synchronize account variable with current account', () => {
-        // GIVEN
-        mockAccountService.identity = jest.fn(() => of(account));
+  describe('login', () => {
+    it('Should call loginService.login on login', () => {
+      // WHEN
+      comp.login();
 
-        // WHEN
-        comp.ngOnInit();
-
-        // THEN
-        expect(comp.account).toEqual(account);
-      });
-    });
-
-    describe('login', () => {
-      it('Should call loginService.login on login', () => {
-        // WHEN
-        comp.login();
-
-        // THEN
-        expect(mockLoginService.login).toHaveBeenCalled();
-      });
+      // THEN
+      expect(mockLoginService.login).toHaveBeenCalled();
     });
   });
 });
